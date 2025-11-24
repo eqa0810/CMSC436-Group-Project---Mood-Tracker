6b4818f4bedea16e696caa2d9a01c4bcb562068d
nnagelia <109233654+namanNagelia@users.noreply.github.com>
2025-11-24 14:13:57 -0500

    Added navigation, settings page, local storage for nickname, and firebase!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -38,6 +40,9 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-firestore")
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
--- app/src/main/java/com/example/group_project/SettingsFragment.kt
+++ app/src/main/java/com/example/group_project/SettingsFragment.kt
@@ -1,5 +1,34 @@
 package com.example.group_project
 
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.example.group_project.repository.UserPreferencesRepository
 
-class SettingsFragment : Fragment(R.layout.fragment_settings)
+class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
+    private lateinit var userPrefsRepository: UserPreferencesRepository
+    private lateinit var nicknameEditText: EditText
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+        userPrefsRepository = UserPreferencesRepository(requireContext())
+        nicknameEditText = view.findViewById(R.id.edittext_nickname)
+        
+        loadUserName()
+        setupListeners()
+    }
+
+    private fun loadUserName() {
+        nicknameEditText.setText(userPrefsRepository.userName)
+    }
+
+    private fun setupListeners() {
+        nicknameEditText.addTextChangedListener {
+            userPrefsRepository.userName = it.toString()
+        }
+    }
+}
